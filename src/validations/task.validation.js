b37c7ed07dbafc6636bd9cf1d2d1ce4cab53da48
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-08-17 00:59:10 +0800

    refactor: update joi validation for task

--- src/validations/task.validation.js
+++ src/validations/task.validation.js
@@ -1,17 +1,29 @@
 const Joi = require('joi');
-const { TASK_TYPES, TASK_STATUSES, TASK_DIFFICULTIES, TASK_FREQUENCIES } = require('../constants');
+const { TASK_TYPES, TASK_STATUSES, TASK_DIFFICULTIES, TASK_FREQUENCIES, TASK_TYPE } = require('../constants');
 const JoiObjectId = require('./helpers/mongoObjectId');
 
 const TaskBody = {
-  name: Joi.string().required(),
-  description: Joi.string(),
+  name: Joi.string().required().description('Task name'),
+  description: Joi.string().allow('', null).description('Task description'),
   type: Joi.string()
     .valid(...TASK_TYPES)
-    .required(),
+    .required()
+    .description('Task type'),
   status: Joi.string().valid(...TASK_STATUSES),
   difficulty: Joi.string().valid(...TASK_DIFFICULTIES),
-  frequency: Joi.string().valid(...TASK_FREQUENCIES),
-  deadlineDate: Joi.date(),
+  frequency: Joi.when('type', {
+    is: TASK_TYPE.DAILIES,
+    then: Joi.string()
+      .valid(...TASK_FREQUENCIES)
+      .required()
+      .description('Task frequency'),
+    otherwise: Joi.forbidden(),
+  }),
+  deadlineDate: Joi.when('type', {
+    is: TASK_TYPE.TODO,
+    then: Joi.date().allow('', null).description('Task deadline date'),
+    otherwise: Joi.forbidden(),
+  }),
 };
 
 const validation = {
@@ -20,28 +32,31 @@ const validation = {
   },
   updateUserTaskById: {
     params: Joi.object().keys({
-      taskId: JoiObjectId().required(),
+      taskId: JoiObjectId().required().description('Task ID'),
     }),
     body: Joi.object().keys(TaskBody),
   },
   patchUserTaskStatusById: {
     params: Joi.object().keys({
-      taskId: JoiObjectId().required(),
+      taskId: JoiObjectId().required().description('Task ID'),
     }),
-    query: Joi.object().keys({
+    body: Joi.object().keys({
       status: Joi.string()
         .valid(...TASK_STATUSES)
-        .required(),
+        .required()
+        .description('Task status'),
     }),
   },
   getUserTaskById: {
     params: Joi.object().keys({
-      taskId: JoiObjectId().required(),
+      taskId: JoiObjectId().required().description('Task ID'),
     }),
   },
   getUserTasks: {
     query: Joi.object().keys({
-      type: Joi.string().valid(...TASK_TYPES),
+      type: Joi.string()
+        .valid(...TASK_TYPES)
+        .description('Task type'),
     }),
   },
 };
