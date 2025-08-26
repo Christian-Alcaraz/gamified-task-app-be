992ed9fe402d1bd105ae104ae9d17b2d649de238
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-08-27 04:00:24 +0800

    feat: added user character validation

--- src/validations/index.js
+++ src/validations/index.js
@@ -1,9 +1,11 @@
 const authValidation = require('./auth.validation');
 const userValidation = require('./user.validation');
+const userCharacterValidation = require('./userCharacter.validation');
 const taskValidation = require('./task.validation');
 
 module.exports = {
   authValidation,
   userValidation,
+  userCharacterValidation,
   taskValidation,
 };
