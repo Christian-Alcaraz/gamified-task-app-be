6707183ea25d50308244ed37e9a7ec76f61a223f
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-09-02 21:15:47 +0800

    feat: added config and game constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,14 +2,18 @@ const sex = require('./sex.constant');
 const status = require('./status.constant');
 const tokenType = require('./tokenType.constant');
 const userType = require('./userType.constant');
+const config = require('./config.constant');
+
+const GAME = require('./game.constant');
 const taskConstants = require('./task.constant');
 const CHARACTER = require('./character.constant');
-
 module.exports = {
   ...sex,
   ...status,
   ...tokenType,
   ...userType,
-  ...taskConstants,
+  ...config,
+  ...taskConstants, // Todo: make this to TASK; so TASK.TYPE TASK.DIFFICULTY
+  GAME,
   CHARACTER,
 };
