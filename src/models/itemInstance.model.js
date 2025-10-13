d672e0d33d124724d785a9fd4a76b9584172b269
Christian Alcaraz <christianmalcaraz@gmail.com>
2025-10-13 15:30:44 +0800

    feat: added itemInstance model
    This is for item that is "instantiated" by the user/task; It is for calculated/rolled stats and rarity

---
+++ src/models/itemInstance.model.js
@@ -0,0 +1,102 @@
+//@ts-check
+const mongoose = require('mongoose');
+const { ITEM } = require('../constants');
+
+/**
+ * @typedef {Object} ItemInstance
+ * @property {string} name
+ * @property {string} description
+ * @property {string} texture
+ * @property {string} type
+ * @property {string} [tags]
+ * @property {Object} [attributes]
+ * @property {Object} [usageAttributes]
+ * @property {string} [cost]
+ * @property {string} currentStackSize
+ * @property {string} maxStackSize
+ * @property {Array<string>} sources
+ */
+
+const itemInstanceSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    texture: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ITEM.TYPES,
+      required: true,
+    },
+    /**
+     * {
+     *    [statName]: [value] - sum of baseStats and the rolled values of item definition (item model)
+     *    strength: 10
+     * }
+     */
+    stats: {
+      type: mongoose.Schema.Types.Mixed,
+    },
+    attributes: {
+      stackable: Boolean,
+      // craftable: Boolean,
+      // enchantable: Boolean,
+      // tradeable: Boolean,
+      // equippable: Boolean,
+      // repairable: Boolean,
+      // sockets: Boolean,
+    },
+    usageAttributes: {
+      consumeOnUse: Boolean,
+      cooldownDuration: Number,
+      effectDuration: Number,
+    },
+    cost: {
+      type: Number,
+    },
+    currentStackSize: {
+      type: Number,
+    },
+    maxStackSize: {
+      type: Number,
+    },
+    tags: {
+      type: mongoose.Schema.Types.Array,
+    },
+    _itemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Item',
+      required: true,
+    },
+    _user: {
+      name: {
+        type: String,
+        required: true,
+      },
+      id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+    },
+    updatedBy: {
+      name: String,
+      userId: mongoose.Schema.Types.ObjectId,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const ItemInstance = mongoose.model('ItemInstance', itemInstanceSchema);
+
+module.exports = ItemInstance;
